28be2508095f028d49910fa1f1902c6e7d8a2192
Chelo84 <marcelo_lacroix@hotmail.com>
2019-06-16 14:44:57 -0300

    fix some parsing issues

--- animecrawler/parser/mal_item_parser.py
+++ animecrawler/parser/mal_item_parser.py
@@ -14,9 +14,11 @@ class MALItemParser:
         item['image'] = response.xpath('//*[@id="content"]//img[@itemprop="image"]/@src').get(default='')
         item['name'] = ExtractUtils.extract_x_path_default_blank(response, '//*[@id="contentWrapper"]/div[1]/h1/span/text()')
         item['type'] = ExtractUtils.extract_x_path_default_blank(response, '//div/span[@class="dark_text" and text()="Type:"]/../a/text()')
-        item['episodes'] = MALItemParser.__reduce_str(response.xpath('//div/span[@class="dark_text" and text()="Episodes:"]/../text()').getall())
+        try:
+            item['episodes'] = int(MALItemParser.__reduce_str(response.xpath('//div/span[@class="dark_text" and text()="Episodes:"]/../text()').getall()))
+        except ValueError:
+            item['episodes'] = 0
         item['status'] = MALItemParser.__reduce_str(response.xpath('//div/span[@class="dark_text" and text()="Status:"]/../text()').getall())
-
         aired = MALItemParser.__parse_aired(response)
         item['aired_from'] = aired['from']
         item['aired_to'] = aired['to']
@@ -29,9 +31,19 @@ class MALItemParser:
         item['genres'] = response.xpath('//div/span[@class="dark_text" and text()="Genres:"]/../a/text()').getall()
         item['duration'] = MALItemParser.__reduce_str(response.xpath('//div/span[@class="dark_text" and text()="Duration:"]/../text()').getall())
         item['rating'] = MALItemParser.__reduce_str(response.xpath('//div/span[@class="dark_text" and text()="Rating:"]/../text()').getall())
-        item['score'] = ExtractUtils.extract_x_path_default_blank(response, '//div[contains(@class, "anime-detail-header-stats")]/div[contains(@class, "stats-block")]/div[@data-title="score"]/text()')
-        item['scored_by'] = ExtractUtils.extract_x_path_default_blank(response, '//div[contains(@class, "anime-detail-header-stats")]/div[contains(@class, "stats-block")]/div[@data-title="score"]/@data-user')
-        item['ranked'] = response.xpath('//div[contains(@class, "anime-detail-header-stats")]/div[contains(@class, "stats-block")]//span[contains(@class, "ranked")]/strong/text()').get(default='N/A').replace('#', '')
+        try:
+            item['score'] = float(ExtractUtils.extract_x_path_default_blank(response, '//div[contains(@class, "anime-detail-header-stats")]/div[contains(@class, "stats-block")]/div[@data-title="score"]/text()'))
+        except ValueError:
+            item['score'] = 0.00
+        try:
+            scored_by = ExtractUtils.extract_x_path_default_blank(response, '//div[contains(@class, "anime-detail-header-stats")]/div[contains(@class, "stats-block")]/div[@data-title="score"]/@data-user')
+            item['scored_by'] = int(scored_by.replace('users', '').replace(',', '').strip())
+        except ValueError:
+            item['scored_by'] = 0
+        try:
+            item['ranked'] = int(response.xpath('//div[contains(@class, "anime-detail-header-stats")]/div[contains(@class, "stats-block")]//span[contains(@class, "ranked")]/strong/text()').get(default='N/A').replace('#', ''))
+        except ValueError:
+            item['ranked'] = 0
         item['popularity'] = int(response
                                  .xpath('//div[contains(@class, "anime-detail-header-stats")]/div[contains(@class, "stats-block")]//span[contains(@class, "popularity")]/strong/text()').get(default='0').replace('#', ''))
         item['members'] = int(response
