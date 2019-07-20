3fc3dc38633e731e8f3a6aac96c84f39fa92f12f
Chelo84 <marcelo_lacroix@hotmail.com>
2019-07-20 15:43:59 -0300

    fix issue where the spider would go just in the first two pages

--- animecrawler/spiders/mal_spider.py
+++ animecrawler/spiders/mal_spider.py
@@ -1,4 +1,8 @@
+import urllib.error
+from urllib.request import urlopen
+
 import scrapy
+
 from animecrawler.parser.mal_item_parser import MALItemParser
 from animecrawler.utils.extract_utils import ExtractUtils
 
@@ -12,6 +16,7 @@ class MALSpider(scrapy.Spider):
 
     def start_requests(self):
         for letter in self.letters:
+            print('version 1.0')
             yield scrapy.Request(url='{url}{letter}'.format(url=self.anime_url, letter=letter), callback=self.parse)
 
     def parse(self, response):
@@ -21,7 +26,22 @@ class MALSpider(scrapy.Spider):
         for url in urls:
             yield scrapy.Request(url=url, callback=MALItemParser.parse_anime)
 
-        next_page_path = response.xpath('//*[@id="content"]/div[5]/div/div/span[normalize-space(text()[1])!=""]/text()[1]/following-sibling::a/@href').get()
-        if next_page_path:
-            next_page = '{hostname}{path}'.format(hostname=ExtractUtils.MAL_URL, path=next_page_path)
+        letter_active = response.xpath(
+            '//div[@id="horiznav_nav"]/ul/li/a[contains(@class, "horiznav_active")]//text()').get()
+        if letter_active and letter_active == '#':
+            letter_active = '.'
+
+        url = response.request.url
+        try:
+            limit = int(url[url.index('show=') + 5:len(url)]) + self.ANIMES_PER_PAGE
+        except ValueError:
+            limit = self.ANIMES_PER_PAGE
+
+        next_page = '{hostname}{path}{letter}&show={limit}'.format(hostname=ExtractUtils.MAL_URL,
+                                                                   path='/anime.php?letter=', letter=letter_active,
+                                                                   limit=limit)
+        try:
+            urlopen(next_page)
             yield scrapy.Request(url=next_page, callback=self.parse)
+        except urllib.error.HTTPError:
+            print("done with the letter {} with {} limit".format(letter_active, limit - self.ANIMES_PER_PAGE))
